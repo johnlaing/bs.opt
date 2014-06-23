c9cbdc7ccb2e3e682db11379237581ccabde0259
John Laing <john.laing@gmail.com>
2014-06-23 10:24:02 -0400

    add fx variant

--- R/bs.opt.R
+++ R/bs.opt.R
@@ -1,5 +1,5 @@
 bs.opt <- function(cp=c("call", "put"), strike, time.to.mat, spot, vol, rate) {
-    stopifnot(cp %in% c("call", "put"))
+    cp <- match.arg(cp, several.ok=TRUE)
     ans <- data.frame(cp, strike, time.to.mat, spot, vol, rate, stringsAsFactors=FALSE)
     d1 <- d1(strike, time.to.mat, spot, vol, rate)
     d2 <- d2(strike, time.to.mat, spot, vol, rate)
@@ -21,6 +21,15 @@ bs.opt <- function(cp=c("call", "put"), strike, time.to.mat, spot, vol, rate) {
     ans$theta <- NA_real_
     ans$theta[cc] <- -spot*dnorm(d1)*vol / (2*sqrt(time.to.mat)) - rate*strike*exp(-rate*time.to.mat) * pnorm(d2)
     ans$theta[!cc] <- -spot*dnorm(d1)*vol / (2*sqrt(time.to.mat)) + rate*strike*exp(-rate*time.to.mat) * pnorm(-d2)
+
+    return(ans)
+}
+
+bs.fx.opt <- function(cp=c("call", "put"), strike, time.to.mat, spot, vol, rate) {
+    ans <- bs.opt(cp=cp, strike=strike, time.to.mat=time.to.mat, spot=spot, vol=vol, rate=rate)
+    ans$price <- ans$price / ans$spot
+    ans$delta <- ans$delta - ans$price
+    ans$gamma <- ans$vega <- ans$theta <- NULL
 
     return(ans)
 }
